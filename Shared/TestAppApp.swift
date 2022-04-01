44fa4e5aac7cb5f630ae2c2f830d8a32ed9db18b
Quang <quanghuylytran@gmail.com>
2022-04-01 13:04:34 +0700

    hihi

---
+++ Shared/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  Shared
+//
+//  Created by Huy Quang on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
